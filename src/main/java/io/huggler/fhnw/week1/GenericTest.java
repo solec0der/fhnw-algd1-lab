18eb755b7e099dff07aa4415a8a6348e64e25252
Yannick Huggler <yannick@huggler.io>
2021-09-26 19:49:13 +0200

    Initial Commit

---
+++ src/main/java/io/huggler/fhnw/week1/GenericTest.java
@@ -0,0 +1,8 @@
+package io.huggler.fhnw.week1;
+
+public class GenericTest<T> {
+
+    T test() {
+        return null;
+    }
+}
---
+++ src/main/java/io/huggler/fhnw/week1/Test.java
@@ -0,0 +1,11 @@
+package io.huggler.fhnw.week1;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = x++ + x++;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
